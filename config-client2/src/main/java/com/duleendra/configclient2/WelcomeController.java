a8ce5c11c09df87bcf2ce2714b8039a3b97c28a0
shashimal <shasimald@gmail.com>
2018-11-23 09:40:17 +0800

    Updated properties files

---
+++ config-client2/src/main/java/com/duleendra/configclient2/WelcomeController.java
@@ -0,0 +1,19 @@
+package com.duleendra.configclient2;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class WelcomeController
+{
+    @Value("${app.service-name:Hello default}")
+    private String serviceName;
+
+    @GetMapping("/service")
+    public String getServiceName() {
+        return "service name [" + this.serviceName + "]";
+    }
+}
