e0a4d1f18dde50c812cee7ac4f54ada9cc6d63af
shashimal <shasimald@gmail.com>
2018-11-23 08:35:49 +0800

    Added the config server folders

---
+++ src/main/java/com/duleendra/springcloudconfigbuspoc/SpringCloudConfigBusPocApplication.java
@@ -0,0 +1,12 @@
+package com.duleendra.springcloudconfigbuspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigBusPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigBusPocApplication.class, args);
+    }
+}
---
+++ src/test/java/com/duleendra/springcloudconfigbuspoc/SpringCloudConfigBusPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.duleendra.springcloudconfigbuspoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudConfigBusPocApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
